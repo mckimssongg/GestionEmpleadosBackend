d34cca0b3c5e23c51e632e9c3abc53cf32a08c43
mckimssongg <soymckimsson286@gmail.com>
2022-10-16 14:43:49 -0600

    crud-empleado

--- src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
+++ src/main/java/com/gestion/empleados/controlador/EmpleadoControlador.java
@@ -1,11 +1,15 @@
 package com.gestion.empleados.controlador;
 
+import com.gestion.empleados.excepciones.ResourceNotFoundException;
 import com.gestion.empleados.modelo.Empleado;
 import com.gestion.empleados.repositorio.EmpleadoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -25,5 +29,40 @@ public class EmpleadoControlador {
     @PostMapping("/empleados")
     public  Empleado guardarEmpleado(@RequestBody Empleado empleado){
         return repositorio.save(empleado);
+    }
+
+    //Buscar un empleado por id
+    @GetMapping("empleados/{id}")
+    public ResponseEntity<Empleado> obtenerUnEmpleadoPorId(@PathVariable Long id){
+        Empleado empleado = repositorio.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException(("No existe un registro con id: "+ id))
+        );
+        return ResponseEntity.ok(empleado);
+    }
+
+    //Actualizar un empleado
+    @PutMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Long id,@RequestBody Empleado detallesEmpleado){
+        Empleado empleado = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No existe el empleado con el ID : " + id));
+
+        empleado.setNombre(detallesEmpleado.getNombre());
+        empleado.setApellido(detallesEmpleado.getApellido());
+        empleado.setEmail(detallesEmpleado.getEmail());
+
+        Empleado empleadoActualizado = repositorio.save(empleado);
+        return ResponseEntity.ok(empleadoActualizado);
+    }
+
+    //Eliminar un empleado
+    @DeleteMapping("/empleados/{id}")
+    public ResponseEntity<Map<String,Boolean>> eliminarEmpleado(@PathVariable Long id){
+        Empleado empleado = repositorio.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No existe el empleado con el ID : " + id));
+
+        repositorio.delete(empleado);
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("eliminar",Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
     }
 }
